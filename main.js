fd5a70d414a5ff0f8ae047912bc9637c48dc8e89
吴雪 <xuewu@wholeeprime.com>
2023-01-30 15:49:24 +0800

    [add]自定义单位和货币

--- main.js
+++ main.js
@@ -4,12 +4,95 @@
  * Use React without JSX
  */
 const e = React.createElement;
+const { useState, useEffect } = React;
+
+function Inputs({
+  unitName,
+  setUnitName,
+  currency,
+  setCurrency
+}) {
+
+  const handleBlur = (e) => {
+    localStorage.setItem('currency') = e.target.value || ''
+  }
+
+  return e(
+    'section',
+    null,
+    e(
+      'label',
+      { className: 'input-label' },
+      'Sale Unit/销售单位:',
+      e(
+        'input',
+        { type: 'text', value: unitName, onChange: (e) => setUnitName(e.target.value) },
+        null
+      )
+    ),
+    e(
+      'label',
+      { className: 'input-label' },
+      'Currency/货币单位:',
+      e(
+        'input',
+        { type: 'text', value: currency, onChange: (e) => setCurrency(e.target.value), onBlur: handleBlur },
+        null
+      )
+    )
+  )
+}
+
+function CompareTabe({
+  unitName,
+  currency
+}) {
+  return e(
+    'table',
+    { className: 'table' },
+    e(
+      'thead',
+      null,
+      e(
+        'tr',
+        null,
+        e(
+          'th',
+          null,
+          unitName
+        ),
+        e(
+          'th',
+          null,
+          currency
+        ),
+        e(
+          'th',
+          null,
+          `${currency}/${unitName}`
+        )
+      )
+    ),
+  )
+}
 
 function App() {
+  const [unitName, setUnitName] = useState('')
+  const [currency, setCurrency] = useState(localStorage.getItem('currency') || '')
+
   return e(
     'div',
     { className: 'container' },
-    'hello word!'
+    e(Inputs, {
+      unitName,
+      setUnitName,
+      currency,
+      setCurrency
+    }),
+    e(CompareTabe, {
+      unitName,
+      currency
+    })
   )
 }
 
